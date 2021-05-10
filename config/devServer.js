34c349f21d766e24e7ce582a5b5c136709eec8e3
pengyishuang <pengyishuang@puduedu.com>
2021-05-10 10:40:01 +0800

    0510

--- config/devServer.js
+++ config/devServer.js
@@ -2,7 +2,8 @@ module.exports = {
   host: "0.0.0.0",
   proxy: {
     '^/testcase': {
-      target: 'http://127.0.0.1:8080',
+      // target: 'http://127.0.0.1:8080',
+      target: 'http://10.0.10.5:8080',
       changeOrigin: true
     }
   }
